353ea74a88a1965bc370b042ef23c75ca907040e
Amol Kote <88299010+amolkote@users.noreply.github.com>
2025-12-08 12:19:11 +0000

    Arrays basic

---
+++ Basics02/Arrays.js
@@ -0,0 +1,11 @@
+let myArray =[0,1,2,3,5];
+console.log(myArray[1]);
+
+let secondArray = new Array();
+     secondArray.push(1);
+     secondArray.push('Amol');
+     secondArray.push('kote');
+      secondArray.pop()
+      secondArray.unshift(9);
+      secondArray.shift();
+     console.log(secondArray);
